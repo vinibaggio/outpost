976812ea202a221d69ccbe0c5397b886812d4ec0
Vinicius Baggio <vinibaggio@gmail.com>
2011-01-19 01:09:05 -0200

    Adding an integration test which uses new case for reports

--- test/integration/basic_dsl_test.rb
+++ test/integration/basic_dsl_test.rb
@@ -3,14 +3,22 @@ require 'test_helper'
 require 'outpost/scouts/http_scout'
 
 class BasicDSLTest < Test::Unit::TestCase
-  class ExampleOne < Outpost::DSL
+  class ExampleSuccess < Outpost::DSL
     depends Outpost::Scouts::HttpScout => 'master http server' do
       options :host => 'example.com'
       report :up, :response_code => 200
     end
   end
 
+  class ExampleFailure < Outpost::DSL
+    depends Outpost::Scouts::HttpScout => 'master http server' do
+      # Google will return 301, so it will fail
+      options :host => 'google.com'
+      report :up, :response_code => 200
+    end
+  end
+
   def test_run_example
-    assert_equal :up, ExampleOne.new.run
+    assert_equal :down, ExampleFailure.new.run
   end
 end
