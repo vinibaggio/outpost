21438db9f812c6baf391165e52c8b994e423ccda
Vinicius Baggio <vinibaggio@gmail.com>
2011-01-19 23:15:40 -0200

    Refactoring as proposed by seacreature. Much better\!

--- lib/outpost/report.rb
+++ lib/outpost/report.rb
@@ -10,15 +10,8 @@ module Outpost
     # if passed [:up, :up, :up], will result on :up
     # if passed [:up, :down, :up], will result on :down
     def self.sumarize(status_list)
-      # FIXME I think there's a way to do this on the logic itself
-      # but will get back on this later
-      return :down if status_list.empty?
-
-      final_status = status_list.inject(true) do |result, status|
-        result &&= (status == :up)
-      end
-
-      final_status ? :up : :down
+      return :down if status_list.empty? || status_list.any? { |s| s == :down }
+      return :up
     end
   end
 end
