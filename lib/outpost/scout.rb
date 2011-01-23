32c00a9c55031fb81b87f1e98d7d56838b6e562a
Vinicius Baggio <vinibaggio@gmail.com>
2011-01-23 12:47:31 -0200

    Added support for hooks

--- lib/outpost/scout.rb
+++ lib/outpost/scout.rb
@@ -1,5 +1,23 @@
 module Outpost
   class Scout
+    class << self
+
+      def hooks
+        @hooks.dup
+      end
+
+      private
+      # Should be only callable by the class itself and not externally
+      def register_hook(hook, callable)
+        if callable.respond_to?(:call)
+          @hooks ||= {}
+          @hooks[hook] = callable
+        else
+          raise ArgumentError, 'Object must respond to method #call to be a valid hook.'
+        end
+      end
+    end
+
     def initialize(description, config)
       @description = description
       @config      = config
@@ -11,7 +29,7 @@ module Outpost
       response = execute
       @config.reports.each do |response_pair, status|
         response_pair.each do |attribute, value|
-          if send(attribute) == value
+          if self.class.hooks[attribute].call(self, value)
             statuses << status
           end
         end
@@ -23,6 +41,7 @@ module Outpost
     def setup(*args)
       raise NotImplementedError, 'You must implement the setup method for Scout to work correctly.'
     end
+
     def execute(*args)
       raise NotImplementedError, 'You must implement the execute method for Scout to work correctly.'
     end
--- test/outpost/scout_test.rb
+++ test/outpost/scout_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 require 'ostruct'
 
 class ScoutTest < Test::Unit::TestCase
+  NoisyError = Class.new(StandardError)
+
   class ScoutExample < Outpost::Scout
     attr_accessor :response
+    register_hook :response, lambda {|scout, status| scout.response == status }
+
     def setup(*args); end
     def execute(*args); end
   end
@@ -20,12 +24,64 @@ class ScoutTest < Test::Unit::TestCase
     assert_equal :down, scout.run
   end
 
+  def test_registered_hook
+    assert_not_nil ScoutExample.hooks[:response]
+  end
+
+  def test_register_invalid_hook
+    assert_raise ArgumentError do
+      add_hook(:invalid_hook, {})
+    end
+  end
+
+  def test_register_valid_hook
+    add_hook(:valid_hook, lambda{|b| b})
+
+    assert_not_nil ScoutExample.hooks[:valid_hook]
+  end
+
+  def test_try_to_modify_hooks
+    ScoutExample.hooks[:another_hook] = {}
+    assert_nil ScoutExample.hooks[:another_hook]
+  end
+
+  def test_try_to_register_hook_from_outside
+    assert_raise NoMethodError do
+      ScoutExample.register_hook :a, lambda {}
+    end
+  end
+
+  def test_do_not_call_noisy_if_no_rules_were_made
+    add_hook(:noisy, proc {|s,r| raise NoisyError})
+    assert_nothing_raised NoisyError do
+      scout = ScoutExample.new("a scout", config_mock)
+      scout.run
+    end
+  end
+
+  def test_call_noisy_if_rules_were_made
+    add_hook(:noisy, proc {|s,r| raise NoisyError})
+    config = config_mock
+    config.reports[{:noisy => nil}] = :down
+
+    assert_raise NoisyError do
+      scout = ScoutExample.new("a scout", config)
+      scout.run
+    end
+  end
+
   private
   def config_mock
     OpenStruct.new.tap do |config|
       config.reports = {}
       config.reports[{:response => true}] = :up
       config.reports[{:response => false}] = :down
+    end
+  end
+
+  def add_hook(hook, callable)
+    ScoutExample.class_eval do
+      register_hook hook, callable
     end
   end
 end
